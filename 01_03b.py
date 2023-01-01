58fc62d8cc80c95a3590ba9d953c6c67068a5d97
alejandromunizluis <61937175+alejandromunizluis@users.noreply.github.com>
2023-01-01 20:26:55 +0000

    module 2, 1st commit

--- 01_03b.py
+++ 01_03b.py
@@ -1,8 +1,15 @@
+from collections import Counter
+
 def main():
     #Add code here
     inventory = Counter(STA001=10, SAL002=20, ENT004=13)
     #sell 5 starters, 3 salads, and 3 entrees
-    #make 9 more starters and 1 more entree
+    inventory_substraction = Counter(STA001=5, SAL002=3, ENT004=3)
+    inventory.subtract(inventory_substraction)
+    # make 9 more starters and 1 more entree
+    inventory_update = Counter(STA001=9, ENT004=1)
+    inventory.update(inventory_update)
+
     print(inventory)
 
 if __name__ == "__main__":
--- 02_01b.py
+++ 02_01b.py
@@ -1,7 +1,18 @@
+from collections import deque
 
 def main():
     #add code here
-    return
+    storage = deque(maxlen=5)
+    storage.append("apple")
+    storage.append("pears")
+    storage.append("orange")
+    storage.append("strawberry")
+    storage.append("pineapple")
+    print(storage)
+    storage.append("coconut")
+    storage.appendleft("peach")
+    print(storage)
+    return storage
 
 if __name__ == "__main__":
     main()
