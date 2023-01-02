7aa7736df2e0e4c9793d24d9cf6d019993902000
alejandromunizluis <61937175+alejandromunizluis@users.noreply.github.com>
2023-01-02 22:25:35 +0000

    deque commit

--- 02_02b.py
+++ 02_02b.py
@@ -1,7 +1,31 @@
 
+from collections import deque
+
+def palindrome_checker(word):
+    w = deque(word)
+    len_w = len(w)
+    isPalindrome = True
+
+    while len_w > 1:
+        w_init = w.popleft().lower()
+        w_fin = w.pop().lower()
+
+        if w_init != w_fin:
+            isPalindrome = False
+
+        len_w = len(w)
+        print(len_w)
+    return isPalindrome
+
 def main():
     #add code here
-    return
+    word = "Tacocat"
+    check = palindrome_checker(word)
+
+    if check:
+        print("The word '{}' is a palindrome".format(word))
+    else:
+        print("The word '{}' is not a palindrome".format(word))
 
 if __name__ == "__main__":
     main()
--- 02_03b.py
+++ 02_03b.py
@@ -1,7 +1,28 @@
 
+from collections import deque
+
+class Task(object):
+    def __init__(self, taskDesc, hasPriority):
+        self.taskDesc = taskDesc
+        self.hasPriority = hasPriority
+    def __str__(self):
+        return "Task: {0}, Priority: {1}".format(self.taskDesc, self.hasPriority)
+
+def add_task(tasklist, tasks, haspriority):
+    return
+
+def complete_task(task, tasklist):
+    return
+
+def print_tasks(tasklist):
+    return print(tasklist)
+
 def main():
     #add code here
-    return
+    tasklist = deque()
+    new_task = ["DJ", "Flowers", "Catering", "Church"]
+    priorities = ["Y", "N", "Y", "N"]
+    print(Task("DJ", True))
 
 if __name__ == "__main__":
     main()
