c62aeb32379e44106c12c96dd245f833c2bba416
alejandromunizluis <61937175+alejandromunizluis@users.noreply.github.com>
2023-01-05 20:22:37 +0000

    otro commit loco

--- 03_01b.py
+++ 03_01b.py
@@ -1,10 +1,14 @@
 
+from collections import namedtuple
+
 def main():
     #add code here
     #create a driver with a name, car type, and car capacity
     #an example you can use to practice: "Iris", "Toyota Prius", 7
     #check if they can take a certain order, given their car's capacity.
-    return
+    drivers = namedtuple('drivers', ['name', 'car', 'capacity'])
+    d = drivers('Iris', 'Toyota Prius', 7)
+    return print(d.name)
 
 if __name__ == "__main__":
     main()
